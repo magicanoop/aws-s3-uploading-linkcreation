88e2a501fb909dc08e9525405d09f37a72a2d4a8
magicanoop <40632403+magicanoop@users.noreply.github.com>
2022-04-12 12:11:49 +0530

    No vendor folder
    Vendor folder should also be added to work. For that need to install aws-php sdk

---
+++ show.php
@@ -0,0 +1,25 @@
+<?php
+require 'vendor/autoload.php';
+
+use Aws\S3\S3Client;  
+use Aws\Exception\AwsException;
+
+$s3Client = new Aws\S3\S3Client([
+    'profile' => 'default',
+    'region' => 'ap-south-1',
+    'version' => '2006-03-01',
+]);
+
+
+//Creating a presigned URL
+$cmd = $s3Client->getCommand('GetObject', [
+    'Bucket' => 'neyyar-mediawall',
+    'Key' => 'test_example/sampletextfile.txt'
+]);
+
+$request = $s3Client->createPresignedRequest($cmd, '+20 minutes');
+
+// Get the actual presigned-url
+$presignedUrl = (string)$request->getUri();
+
+echo $presignedUrl;
